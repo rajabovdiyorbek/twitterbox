5a2711604ab2e54485ce5ccb7fb5cdebaf9790ae
Diyorbek <rajabov.diyorbek.it@gmail.com>
2022-12-22 22:11:08 +0500

    firstPlagin

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,100 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>Project Twitter Box Client</h1>
+
+    <form v-on:submit.prevent class="box">
+      <textarea
+      v-model.trim="tweet"
+      v-bind:maxlength="characters"
+      type="text" 
+      />
+      <button 
+      v-show="tweet.length > 0"
+      @click="addTweet"
+      :disabled="!maxC"
+      >
+      Submit
+      </button>
+    </form>
+    <h4 :class="{ 'red' : !maxC}">
+      characters remaining {{ maxC }}
+    </h4>
+    <div :class="{'tweet' : tweet.length > 0}" v-for="(tweet, index) in tweets" v-bind:key="index">
+      <h4>{{ tweet }}</h4>
+      <button v-if="tweet.length > 0" @click="removeTweet(index)">delete</button>
+    </div>
   </div>
 </template>
 
+
+
+
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  data() {
+    return {
+      maxCharacters: '',
+      characters: 40,
+      tweets: [],
+      tweet: ''
+    }
+  },
+  methods: {
+    addTweet() {
+      this.tweets.push(this.tweet)
+      this.tweet = ''
+    },
+    removeTweet(index) {
+      this.tweets.splice(index, 1)
+    }
+  },
+  computed: {
+    maxC() {
+      return this.characters - this.tweet.length
+    },
+
   }
 }
 </script>
 
+
+
+
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.box {
+  width: 33%;
+  background-color: #b8dcdc;
+  height: 100px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
+    0 10px 10px -5px rgba(0, 0, 0, 0.04);
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.red {
+  color: red;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.tweet {
+  width: 30%;
+  background-color: #b8dcdc;
+  margin: auto;
+  padding: 20px 15px;
+  margin-top: 10px;
+  box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
+      0 10px 10px -5px rgba(0, 0, 0, 0.04);
 }
-a {
-  color: #42b983;
+
+button {
+  margin-left: 10px;
+  padding: 10px 15px;
+  border: none;
+  color: white;
+  cursor: pointer;
+  background-color: rgb(66, 154, 248);
 }
 </style>
